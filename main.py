5f865920d95e00d7c56e8965e144fd4e8b55b6ab
turalt <turalt@educ.college-montreal.qc.ca>
2022-12-21 18:58:38 -0500

    mid

---
+++ main.py
@@ -0,0 +1,16 @@
+#Troy Tural 404
+# Exercise 2
+
+class Rectangle:
+   def __init__(self,largeur,longeur):
+       self.largeur = largeur
+       self.longueur = longeur
+
+
+   def calcul_aire(self):
+       self.calcul_aire = self.largeur * self.longueur
+
+
+   def afficher_infos(self):
+       print(self.calcul_aire)
+
